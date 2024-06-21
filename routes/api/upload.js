c0c7e20df6fc269cd3e94847385526a8e978b54e
Azmir Uddin Alif <azmir02alif@gmail.com>
2024-06-21 16:10:26 +0600

    new

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { uploadImages, listImage } = require('../../controllers/upload')
 const uploadMiddleware = require('../../middleware/uploadMiddleware')
+const { authUser } = require('../../middleware/auth')
 const router = express.Router()
 
-router.post('/uploadimage', uploadMiddleware, uploadImages)
-router.get('/listimage', listImage)
+router.post('/uploadimage', authUser, uploadMiddleware, uploadImages)
+router.post('/listimage', authUser, listImage)
 
 
 module.exports = router
