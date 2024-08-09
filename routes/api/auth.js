70ae706d86595d3781e335374cd86b9fdbce6e66
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2024-08-09 13:28:53 +0600

    new

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { newUser, verifiedUser, login, reVerification, findUser, resetCode, verifyCode, changePassword, getUser, updateProfilePicture, updateCoverPicture, updateDetails, addFriend, cancelRequest, follow, unFollow, acceptRequest, unFriend, deleteRequest, search } = require('../../controllers/userControllers')
+const { newUser, verifiedUser, login, reVerification, findUser, resetCode, verifyCode, changePassword, getUser, updateProfilePicture, updateCoverPicture, updateDetails, addFriend, cancelRequest, follow, unFollow, acceptRequest, unFriend, deleteRequest, search, addSearchHistory } = require('../../controllers/userControllers')
 const { authUser } = require('../../middleware/auth')
 
 router.post('/', newUser)
@@ -23,6 +23,7 @@ router.put('/acceptrequest/:id', authUser, acceptRequest)
 router.put('/unfriend/:id', authUser, unFriend)
 router.put('/deleterequest/:id', authUser, deleteRequest)
 router.post('/search/:searchTerm', authUser, search)
+router.put('/addsearchhistory', authUser, addSearchHistory)
 
 
 module.exports = router
